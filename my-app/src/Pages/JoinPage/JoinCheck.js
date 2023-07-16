bef0a2c1a8decc4351191aa86d0cda543d338378
조경은 <kavvy99@naver.com>
2023-07-17 00:47:05 +0900

    add Join page

---
+++ my-app/src/Pages/JoinPage/JoinCheck.js
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+import CheckBoxItem from '../../Components/Join/JoinCheckBoxItem';
+import JoinHeader from '../../Components/Header/JoinHeader';
+
+export default function JoinCheck() {
+    return(
+        <div>
+          <JoinHeader headerTitle="이용약관 및 개인정보에 관한 동의"/>
+
+          <CheckBoxGroup>
+
+            <CheckItem>
+              <CheckBoxItem content="(필수) 이용약관"/>
+              <CheckWhole>전문보기</CheckWhole>
+            </CheckItem>
+
+            <CheckItem>
+              <CheckBoxItem content="(필수) 개인정보 수집 및 동의"/>
+              <CheckWhole>전문보기</CheckWhole>
+            </CheckItem>
+
+          </CheckBoxGroup>
+
+
+          <AgreeBtnWrapper>
+              <AgreeBtn>전체동의</AgreeBtn>
+          </AgreeBtnWrapper>
+        </div>
+    )
+}
+
+const CheckBoxGroup = styled.div`
+  display: flex;
+  justify-content: left;
+  flex-direction: column;
+  padding: 150px;
+  margin-top: 150px;
+  border-bottom: 1px solid #E8E8E8;
+`;
+const CheckItem = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  margin: 10px;
+`;
+const CheckWhole = styled.div`
+  font-family: "NanumSquareNeoContents";
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 15.47px;
+
+`;
+
+const AgreeBtnWrapper = styled.div`
+  margin: 1.5rem 0;
+  display: flex;
+  justify-content: center;
+`;
+
+const AgreeBtn = styled.button`
+  box-shadow: none;
+  width: 416px;
+  height: 48px;
+  background-color: #ffc000;
+  border: 1px solid #ffc000;
+  border-radius: 4px;
+  cursor: pointer;
+  font-family: "NanumSquareNeoContents";
+
+`;
