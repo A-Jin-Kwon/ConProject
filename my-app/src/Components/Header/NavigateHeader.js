17d4d49482a43648eace21b41efdcf4b90ad7a72
A-Jin-Kwon <rnjsdkwls123@naver.com>
2023-07-25 19:15:48 +0900

    헤더 뒤로가기 화살표 컴포넌트 생성

--- my-app/src/Components/Header/NavigateHeader.js
+++ my-app/src/Components/Header/NavigateHeader.js
@@ -1,19 +1,14 @@
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import PrevArrow from './PrevArrow';
 
 // 헤더에 들어갈 텍스트와 화살표를 눌렀을 때 이동할 라우트를 보내주세요.
 const NavigateHeader = ({headerTitlte, path}) => {
-    const navigate = useNavigate();
-    const clickHandler = () => {
-        navigate(path);
-    }
-
     return(
         <header>
             <Wrapper>
                 <SpaceLeft/>
                 <SpaceLeft>
-                    <Image src="imgs/navigate_next.png" onClick={clickHandler}/>
+                    <PrevArrow path={path}/>
                 </SpaceLeft>
                 <HeaderText>
                     {headerTitlte}
@@ -42,8 +37,4 @@ const HeaderText = styled.div`
     font-color: #242424;
     font-weight: 700;
     font-size: 16px;
-`;
-const Image = styled.img`
-    color: #2e2e2e;
-    cursor: pointer;
 `;
